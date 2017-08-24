9d1f187f895978bd35fe8eb780e8c80befb1915d
Qianhao2579 <31297232+Qianhao2579@users.noreply.github.com>
2017-08-24 09:40:07 +0800

    cam_exp
    1.opencv3.2 摄像头测试

---
+++ cam_exp/example.cpp
@@ -0,0 +1,48 @@
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/videoio.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void drawText(Mat & image);
+
+int main()
+{
+    cout << "Built with OpenCV " << CV_VERSION << endl;
+    Mat image;
+    VideoCapture capture(-1);
+    //capture.open(-1);
+
+    if(capture.isOpened())
+    {
+        cout << "Capture 0 is opened" << endl;
+       while(1)
+        {
+            capture >> image;
+            drawText(image);
+            imshow("Sample", image);
+            waitKey(30);
+        }
+    }
+    else
+    {
+        cout << "No capture" << endl;
+        image = Mat::zeros(480, 640, CV_8UC1);
+        drawText(image);
+        imshow("Sample", image);
+        waitKey(0);
+    }
+   return 0;
+}
+
+void drawText(Mat & image)
+{
+    putText(image, "Hello OpenCV",
+            Point(20, 50),
+            FONT_HERSHEY_COMPLEX, 1, // font face and scale
+            Scalar(255, 255, 255), // white
+            1, LINE_AA); // line thickness and type
+}
